cde8dc543fbbb3bd281fcc643ee89618b23ff35f
huiru1221 <a53262613@gmail.com>
2023-08-01 10:23:06 +0800

    update app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <v-app>
-    <v-app-bar>
-      <v-container class="d-flex align-center">
+    <v-app-bar  :color="bgAppColor">
+      <v-container class="d-flex align-center" >
         <v-app-bar-title>番茄鐘</v-app-bar-title>
+        <div>
+          <div></div>
+          <div hight="200">
+            <label>選擇主題顏色</label>
+            <v-select
+              v-model="bgAppColor"
+              :items="colorOptions"
+              label="選擇主題顏色"
+              variant="outlined"
+            ></v-select>
+          </div>
+        </div>
         <v-btn prepend-icon="mdi-home" to="/">首頁</v-btn>
         <v-btn prepend-icon="mdi-format-list-bulleted" to="/list">事項</v-btn>
         <v-btn prepend-icon="mdi-cog" to="/settings">設定</v-btn>
       </v-container>
     </v-app-bar>
     <v-main>
-      <!-- Component 是目前頁面該顯示的元件 -->
       <router-view v-slot="{ Component }">
-        <!-- keep-alive 讓路由切換時不會銷毀元件，用 include 指定只有 Home 不會被銷毀 -->
-        <!-- component 動態元件，顯示 is 綁定的元件 -->
         <keep-alive include="Home">
           <component :is="Component"></component>
         </keep-alive>
@@ -22,4 +31,9 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+const colorOptions = ['pink', 'blue', 'yellow', 'green']
+
+const bgAppColor = ref('white')
 </script>
